7111133ad69f507dc18f5dd8ec8196ba47959e56
LaDonne Pallanck <ladonne@uw.edu>
2014-11-16 10:54:20 -0800

    final commit probably

--- js/app.js
+++ js/app.js
@@ -3,3 +3,105 @@
     app.js, main Angular application script
     define your module and controllers here
 */
+
+var reviewsUrl = 'https://api.parse.com/1/classes/reviews';
+
+angular.module('TalkingBack', ['ui.bootstrap'])
+    .config(function($httpProvider) {
+        $httpProvider.defaults.headers.common['X-Parse-Application-Id'] = 'igHWCjF6WMhApYBP058UqOR6XAWqe1jWmRqtmXLW';
+        $httpProvider.defaults.headers.common['X-Parse-REST-API-Key'] = '9Ve9rAoGoQxdWXLCvGYaxZmOY5mL0WKqCnqmHa6m';
+    })
+
+    .controller('ReviewController', function($scope, $http) {
+
+        function refreshReview() {
+            $scope.loading = true;
+            $scope.hasReviews = true;
+            $http.get(reviewsUrl + '?order=-votes')
+                .success(function(data) {
+                    $scope.reviews = data.results;
+                    console.log(reviewsUrl);
+                    console.log($scope.reviews);
+                })
+                .error(function(err){
+                    console.log(err);
+                })
+                .finally(function() {
+                    $scope.loading = false;
+                    if ($scope.reviews.length == 0) {
+                        $scope.hasReviews = false;
+                    }
+                });
+        }
+
+        refreshReview();
+
+        $scope.deleteReview = function(review) {
+            $scope.loading = true;
+            $http.delete(reviewsUrl + '/' + review.objectId)
+                .success(function (data) {
+                    refreshReview();
+                })
+                .error(function (err) {
+                    console.log(err);
+                })
+                .finally(function () {
+                    $scope.loading = false;
+                });
+        };
+
+        $scope.newReview = {votes: 0};
+
+        $scope.addReview = function(review) {
+            $scope.inserting = true;//disabled add task button until inserted
+            $http.post(reviewsUrl, review)
+                .success(function(responseData) {
+                    review.objectId = responseData.objectId;
+                    $scope.reviews.push(review);
+                })
+
+                .finally(function() {
+                    $scope.newReview = {votes: 0};
+                    $scope.inserting = false;
+                    refreshReview();
+                });
+        };
+        $scope.updateReview = function(review) {
+            $scope.updating = true;
+            $http.put(reviewsUrl + '/' + review.objectId, review)
+                .success(function() {
+                    //we could give some feedback
+                })
+                .finally(function() {
+                    $scope.updating = false;
+                });
+        };
+
+        $scope.incrementVotes = function(review, amount) {
+            console.log(review.votes);
+            if ((review.votes == 0 && amount == -1) || review.votes < 0) {
+                return false;
+            }
+            var postData = {
+                votes: {
+                    __op: "Increment",
+                    amount: amount
+                }
+            };
+
+
+            $scope.updating = true;
+            $http.put(reviewsUrl + '/' + review.objectId, postData)
+                .success(function (respData) {
+                    review.votes = respData.votes;
+                })
+                .error(function (err) {
+                    console.log(err);
+                })
+                .finally(function () {
+                    $scope.updating = false;
+                    return true;
+                });
+        };
+
+    });
